50d549406945f081c5af56921ae3f76efc4fc4b4
Matt <matt@FuGuiZhu>
2015-12-21 16:02:27 -0800

    =Updated laptop dotfiles, updated install.sh to create different symbolic links on laptop. install.sh also deletes old home directory dotfiles and updates xrdb after creating symbolic links.

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-ln -sv "$(pwd)/.dotfiles/.vimrc" ~
-ln -sv "$(pwd)/.dotfiles/.Xresources" ~
-ln -sv "$(pwd)/.dotfiles/.bash_aliases" ~
+#delete old symbolic links/default dotfiles
+rm ~/.bash_aliases
+rm ~/.vimrc
+rm ~/.Xresources
+
+ln -sv ~/.dotfiles/.bash_aliases ~
+
+#if this is on my laptop, use the alternate dotfiles
+if [ "$(hostname)" == "FuGuiZhu" ]; then
+    ln -sv ~/.dotfiles/.vimrc_laptop ~/.vimrc
+    ln -sv ~/.dotfiles/.Xresources_laptop ~/.Xresources
+else
+    ln -sv ~/.dotfiles/.vimrc ~
+    ln -sv ~/.dotfiles/.Xresources ~
+fi
+
+#update X11 database
+xrdb ~/.Xresources
